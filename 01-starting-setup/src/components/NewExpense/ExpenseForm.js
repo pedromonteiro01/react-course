338cef6057992236a986899fdd0a1c92e56d99b5
pedromonteiro01 <pmapm@ua.pt>
2022-04-02 22:41:56 +0100

    back to multiple states

--- 01-starting-setup/src/components/NewExpense/ExpenseForm.js
+++ 01-starting-setup/src/components/NewExpense/ExpenseForm.js
@@ -3,45 +3,48 @@ import React, { useState } from 'react';
 import "./ExpenseForm.css";
 
 function ExpenseForm() {
-    //const [enteredTitle, setEnteredTitle] = useState('');
-    //const [enteredAmount, setEnteredAmount] = useState('');
-    //const [enteredDate, setEnteredDate] = useState('');
+    const [enteredTitle, setEnteredTitle] = useState('');
+    const [enteredAmount, setEnteredAmount] = useState('');
+    const [enteredDate, setEnteredDate] = useState('');
 
-    const [userInput, setUserInput] = useState({ // when update the state, update all the 3 properties
-        enteredTitle : '',
-        enteredAmount: '',
-        enteredDate: ''
-    });
+    //const [userInput, setUserInput] = useState({ // when update the state, update all the 3 properties
+     //   enteredTitle : '',
+     //   enteredAmount: '',
+    //    enteredDate: ''
+    //});
 
     const titleChangeHandler = (event) => {
+        setEnteredTitle(event.target.value);
         //setUserInput({
             // depends on the last state (NOT A GOOD IDEA)
         //    ...userInput,
         //    enteredTitle: event.target.value,
         //})
-        setUserInput((prevState) => {
-            return {...prevState, enteredTitle: event.target.value}
-        })
+        //setUserInput((prevState) => {
+        //    return {...prevState, enteredTitle: event.target.value}
+        //})
     };
 
     const amountChangeHandler = (event) => {
+        setEnteredAmount(event.target.value);
         //setUserInput({
         //    ...userInput,
         //    enteredAmount: event.target.value,
         //})
-        setUserInput((prevState) => {
-            return {...prevState, enteredAmount: event.target.value}
-        })
+        //setUserInput((prevState) => {
+        //    return {...prevState, enteredAmount: event.target.value}
+        //})
     };
 
     const dateChangeHandler = (event) => {
+        setEnteredDate(event.target.value);
         //setUserInput({
         //    ...userInput,
         //    enteredDate: event.target.value,
         //})
-        setUserInput((prevState) => {
-            return {...prevState, enteredDate: event.target.value} // operates only on the lastest state snapshot
-        })
+        //setUserInput((prevState) => {
+        //    return {...prevState, enteredDate: event.target.value} // operates only on the lastest state snapshot
+        //})
     };
 
     return <form>
