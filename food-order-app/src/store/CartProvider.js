1b1b92254b57c8e631b3a2c5ccbd1d22e86d4616
pedromonteiro01 <pmapm@ua.pt>
2022-07-14 12:11:16 +0100

    add cart reducer

---
+++ food-order-app/src/store/CartProvider.js
@@ -0,0 +1,53 @@
+import { useReducer } from "react";
+import CartContext from "./cart-context";
+
+const defaultCartState = {
+    items: [],
+    totalAmount: 0
+}
+
+const cartReducer = (state, action) => {
+    if (action.type === 'ADD') {
+        const updatedItems = state.items.concat(action.item); //current state snapshot
+        const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+        return {
+            items: updatedItems,
+            totalAmount: updatedTotalAmount
+        };
+    }
+    return defaultCartState;
+};
+
+const CartProvider = (props) => {
+
+    const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState);
+
+    const addItemToCartHandler = (item) => {
+        dispatchCartAction({
+            type:'ADD',
+            item:item
+        });
+    }
+
+    const removeItemFromCartHandler = (item) => {
+        dispatchCartAction({
+            type:'REMOVE',
+            item:item
+        })
+    }
+
+    const cartContext = {
+        items: cartState.items,
+        totalAmount: cartState.totalAmount,
+        addItem: addItemToCartHandler,
+        removeItem: removeItemFromCartHandler 
+    }
+
+    return (
+        <CartContext.Provider value={cartContext}>
+            {props.children}
+        </CartContext.Provider>
+    )
+}
+
+export default CartProvider
