0d6cecd6b223895a9914aab4d884a02cc0651567
pedromonteiro01 <pmapm@ua.pt>
2022-07-21 19:15:02 +0100

    finish redux project, add http requests project

--- redux-project/src/store/index.js
+++ redux-project/src/store/index.js
@@ -1,39 +1,12 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from './counter';
+import authReducer from "./auth";
 
-const initialState = { counter: 0, showCounter: true };
-
-const counterReducer = (state = initialState, action) => {
-  if (action.type === 'increment') {
-    return {
-      counter: state.counter + 1,
-      showCounter: state.showCounter
-    };
-  }
-
-  if (action.type === 'increase') {
-    return {
-      counter: state.counter + action.amount,
-      showCounter: state.showCounter
-    };
-  }
-
-  if (action.type === 'decrement') {
-    return {
-      counter: state.counter - 1,
-      showCounter: state.showCounter
-    };
-  }
-
-  if (action.type === 'toggle') {
-    return {
-      counter: state.counter,
-      showCounter: !state.showCounter
-    };
-  }
-
-  return state;
-};
-
-const store = configureStore({reducer: counterReducer});
+const store = configureStore({
+  reducer: {
+    counter: counterReducer,
+    auth: authReducer,
+  },
+});
 
 export default store;
